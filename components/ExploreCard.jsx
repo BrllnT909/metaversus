e3ca1646fc91ee8a7837565d9a3592bfdeb02a05
BrllnT909 <brllnt.999@gmail.com>
2022-12-01 19:56:24 +0700

    push from giga

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,9 +1,21 @@
 'use client';
+import {useState} from 'react';
+import { motion, MotionConfigContext } from "framer-motion";
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const ExploreCard = () => (
-  <div>
-    Explore Card
-  </div>
+  <section className={`${styles.paddings}`} id="ExploreCard">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport ={{once:false, amount:0.25}}
+      className= {`${styles.innerWidth} mx-auto flex flex-col`}>
+        <TypingText title ="| The World" textStyles='text-center'/>
+
+    </motion.div>
+  </section>
 );
 
 export default ExploreCard;
